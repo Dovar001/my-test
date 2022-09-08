3ce233926f0a565b37dcaee57caa64f777ef9f1e
Dovar <dovarabdulhanonov@gmail.com>
2022-09-08 11:32:51 +0500

    added procent for result

--- src/components/result/result.tsx
+++ src/components/result/result.tsx
@@ -1,11 +1,13 @@
-import { Button } from '@mui/material';
-import { FC } from 'react';
-import { Link, useParams } from 'react-router-dom';
-import { visitorTestQuestions } from '../../tests.fake.data';
-import { ResultContent } from '../result-content';
+import { Button, Grid, Stack } from "@mui/material";
+import { FC, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import { visitorTestQuestions } from "../../tests.fake.data";
+import { ResultContent } from "../result-content";
+import Circle from "react-circle";
+import styles from "./result.module.css";
 
 const Result: FC = () => {
-  const params = useParams();
+  const [viewAnswers, setViewAnswers] = useState<boolean>(false);
 
   const startNew = () => {
     visitorTestQuestions.map((item: any) => {
@@ -13,19 +15,69 @@ const Result: FC = () => {
     });
   };
 
+  const correctAnswer = () => {
+    return visitorTestQuestions?.reduce(
+      (prev: number, curr: any) =>
+        curr?.answerIds[0] === curr.rightAnswerId ? (prev += 1) : prev,
+      0
+    );
+  };
+
+  const answerPercent = Math.round(
+    (correctAnswer() * 100) / visitorTestQuestions?.length
+  );
+
+  console.log("correct", correctAnswer());
+  console.log("length", visitorTestQuestions?.length);
+
   return (
-    <div>
-      <div>
-        {visitorTestQuestions?.map((value: any, index: any) => {
-          return <ResultContent currentQuestion={index + 1} />;
-        })}
-      </div>
-      <Button variant='contained' onClick={() => startNew()}>
-        <Link style={{ textDecoration: 'none', color: '#fff' }} to='/'>
-          Start new test
-        </Link>
-      </Button>
-    </div>
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <Stack
+          border={4}
+          borderColor="green"
+          padding={1}
+          justifyContent="center"
+          direction="row"
+          alignItems="center"
+        >
+          You have answered correctly{" "}
+          <span className={styles.number}>{correctAnswer()}</span> from
+          <span className={styles.number}>{visitorTestQuestions?.length}</span>
+          <Circle
+            textStyle={{
+              font: "bold 5rem Helvetica, Arial, sans-serif",
+            }}
+            progress={answerPercent}
+            animate={true}
+            animationDuration="2s"
+          />
+        </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        <Stack alignItems="flex-end">
+          <Button size="large" variant="contained" onClick={() => startNew()}>
+            <Link style={{ textDecoration: "none", color: "#fff" }} to="/">
+              Start new test
+            </Link>
+          </Button>
+        </Stack>
+      </Grid>
+      <Grid item xs={12}>
+        <Stack spacing={2} direction="column">
+          <Button
+            variant="contained"
+            onClick={() => setViewAnswers(!viewAnswers)}
+          >
+            {!viewAnswers ? "View answers ↓" : "Hide answers ↑ "}
+          </Button>
+          {viewAnswers &&
+            visitorTestQuestions?.map((value: any, index: any) => {
+              return <ResultContent currentQuestion={index + 1} />;
+            })}
+        </Stack>
+      </Grid>
+    </Grid>
   );
 };
 export { Result };
